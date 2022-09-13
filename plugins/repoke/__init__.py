1a8acec74e976110923cf4539dc83f3947a7a335
Isoheptane <chomodiso@outlook.com>
2022-09-13 13:11:35 +0800

    Fix bot poke bot result in rapid poke

--- plugins/repoke/__init__.py
+++ plugins/repoke/__init__.py
@@ -1,10 +1,11 @@
+from time import time
 from nonebot import on_notice
 from nonebot.adapters import Event, Bot
 from nonebot.adapters.onebot.v11 import Event, PokeNotifyEvent, Message, MessageSegment
 
 def check_poke(event: Event):
     if isinstance (event, PokeNotifyEvent):
-        if (event.target_id == event.self_id):
+        if (event.target_id == event.self_id and event.user_id != event.self_id):
             return True
     return False
 
